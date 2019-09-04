503f37ae5cb0101c9c5b1904807394a592bc99c4
Christopher Enytc <chris@enytc.com>
2019-09-04 08:58:54 -0300

    Fix travis build

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GrpcHealthChecker.MixProject do
     [
       app: :grpc_health_checker,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       name: "gRPC Health Checker",
